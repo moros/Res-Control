3970879503c6c0cef8232267f218f58cee454280
Doug Mason <androidsoong@gmailc.om>
2021-01-02 20:35:15 -0600

    Initial Commit

---
+++ Res Switcher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Res Switcher
+//
+//  Created by Doug Mason on 1/2/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
